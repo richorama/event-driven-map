9b2981d89be21bb6f37cd48dc6d7dbb7a9fb2055
Richard Astbury <richard.astbury@gmail.com>
2019-12-20 11:21:30 +1100

    move app to 'docs'

---
+++ docs/precache-manifest.319f75035fa24230970ce488d24220f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7ccec1464946a38d231d6277b8e3dc",
+    "url": "/event-drive-map/index.html"
+  },
+  {
+    "revision": "21ce1dfec01e6c6caa88",
+    "url": "/event-drive-map/static/css/2.7997c105.chunk.css"
+  },
+  {
+    "revision": "5d375c2c7cda9d4ec858",
+    "url": "/event-drive-map/static/css/main.e751ad72.chunk.css"
+  },
+  {
+    "revision": "21ce1dfec01e6c6caa88",
+    "url": "/event-drive-map/static/js/2.2efe5ada.chunk.js"
+  },
+  {
+    "revision": "2877b8a9220b11f3ffa2c43c3a2a4874",
+    "url": "/event-drive-map/static/js/2.2efe5ada.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d375c2c7cda9d4ec858",
+    "url": "/event-drive-map/static/js/main.0286fd3f.chunk.js"
+  },
+  {
+    "revision": "c9ce396adbfb5622a276",
+    "url": "/event-drive-map/static/js/runtime-main.bd8f8fd0.js"
+  }
+]);
